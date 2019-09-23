99dad114702a00dc664712f21487c1410642267f
VasyaSmolyar <smolyar4@gmail.com>
2019-09-23 20:07:20 +0300

    Add a validation

--- src/Login.js
+++ src/Login.js
@@ -1,29 +1,92 @@
 import React from 'react';
 
+const stdLogin = 'Login must consist only of latin letters and numbers';
+const stdPass = 'Password must be longer than 7 characters and contain only latin letters and numbers';
+const errConf = 'Passwords must match';
+var reg = /^[\w\d]+$/;
+
 class Login extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             login: '',
             password: '',
-            confirm: ''
+            confirm: '',
+            loginMes: stdLogin,
+            passMes: stdPass,
+            confirmMes: '',
+            loginClass: 'regular',
+            passClass: 'regular',
+            confClass: 'regular'
         };
+        this.login = this.login.bind(this);
+        this.password = this.password.bind(this);
+        this.confirm = this.confirm.bind(this);
+        this.send = this.send.bind(this);
     }
 
-    login() {
-
+    login(event) {
+        var text = event.target.value;
+        var mes, step;
+        if(text == '' || !reg.test(text)) {
+            mes = stdLogin;
+            step = 'error';
+        } else {
+            mes = '';
+            step = 'great';
+        }
+        this.setState({
+            login: text,
+            loginMes: mes,
+            loginClass: step
+        });
     }
 
-    password() {
-
+    password(event) {
+        var text = event.target.value;
+        var mes, conf, step, confStep = this.state.confClass;
+        if(text == '' || !reg.test(text)) {
+            step = 'error';
+            mes = stdPass;
+        } else if(text.length < 8) {
+            step = 'error';
+            mes = 'Password must be longer than 7 characters';
+        }
+        if (this.state.confirm != '' && this.state.confirm != text) {
+            confStep = 'error';
+            conf = errConf;
+        } else {
+            conf = '';
+            step = 'great';
+        }
+        this.setState({
+            password: text,
+            passMes: mes,
+            confirmMes: conf,
+            passClass: step,
+            confClass: confStep
+        });
     }
 
-    confirm() {
-
+    confirm(event) {
+        var text = event.target.value;
+        var mes, step;
+        if(text != this.state.password) {
+            mes = errConf;
+            step = 'error';
+        } else {
+            mes = '';
+            step = 'great';
+        }
+        this.setState({
+            confirm: text,
+            confirmMes: mes,
+            confirmClass: step
+        });
     }
 
     send() {
-
+        console.log([this.state.loginClass,this.state.passClass,this.state.confirmClass]);
     }
 
     render() {
@@ -31,10 +94,15 @@ class Login extends React.Component {
             <div id="modal">
                 <div>
                     <button onClick={this.props.cancel}>Cancel</button>
-                    <input onChange={this.login}></input>
-                    <input onChange={this.password}></input>
+                    <input onChange={this.login} value={this.state.login} className={this.state.loginClass}/>
+                    <span>{this.state.loginMes}</span>
+                    <input onChange={this.password} value={this.state.password} className={this.state.passClass}/>
+                    <span>{this.state.passMes}</span>
                     {(() => {if(this.props.register) {
-                        return <input onChange={this.confirm}></input>;
+                        return(<span>
+                                <input onChange={this.confirm} value={this.state.confirm} className={this.state.confirmClass} />
+                                <span>{this.state.confirmMes}</span>
+                        </span>);
                     }})()}
                     <button onClick={this.send}>{this.props.name}</button>
                 </div>
